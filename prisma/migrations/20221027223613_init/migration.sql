9c3ae49352e86e77325f48755ae519590f1cc3c9
fanuelhpa <fanuelrique@gmail.com>
2022-10-28 11:28:39 -0300

    Prisma Schema DB imported

--- prisma/migrations/20221027223613_init/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE `State` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name_` VARCHAR(191) NOT NULL,
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
